21dc29d3df102437ef39db5f27d794f2c5080e29
jimmymetts <jimmymetts@bellsouth.net>
2020-07-27 17:02:06 -0500

    added breaststroke pullout to experts

---
+++ src/Experts.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect }  from 'react';
+import { Container } from 'reactstrap';
+import {Rating} from '@material-ui/lab';
+
+function Experts() {
+
+const [value, setValue] = useState();
+const [hover, setHover] = useState();
+return (
+    
+   <Container>
+   <br></br>
+   <br></br>
+    <h1>Experts</h1>
+    <br></br>
+    <br></br>
+    <div className='div-one'>
+     <p> Breaststroke Pull Out
+     
+        <Rating 
+            // className='rating'
+            style={{marginLeft:50}}
+            value={value} 
+            onChange={(e, newValue) => {
+            setValue(newValue);
+          }} 
+            onChangeActive={(e, newHover) => {
+            setHover(newHover);
+          }} >   
+        </Rating>  
+        </p>
+        </div>       
+        <hr/>
+       
+  
+            <form input='text'>Comments/Notes</form>
+            <textarea placeholder='Comments/Notes'></textarea>
+            {/* <Button> Add </Button> */}
+            
+     
+        
+    </Container>
+)
+}
+
+export default Experts
