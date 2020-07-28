138c3035c63f81b81ca6ba7d5523aea126c8f88f
jimmymetts <jimmymetts@bellsouth.net>
2020-07-28 09:18:01 -0500

    added bordered table to beginners.js

--- src/Advanced.js
+++ src/Advanced.js
@@ -1,132 +1,132 @@
-// import React, { useState, useEffect }  from 'react';
-// import { Container } from 'reactstrap';
-// import {Rating} from '@material-ui/lab';
+import React, { useState, useEffect }  from 'react';
+import { Container } from 'reactstrap';
+import {Rating} from '@material-ui/lab';
 
-// function Advanced() {
-//     const [value, setValue] = useState();
-//     const [hover, setHover] = useState();
-//     return (
+function Advanced() {
+    const [value, setValue] = useState();
+    const [hover, setHover] = useState();
+    return (
         
-//        <Container>
-//        <br></br>
-//        <br></br>
-//         <h1>Advanced</h1>
-//         <br></br>
-//         <br></br>
-//         <div className='div-one'>
-//          <p> Breaststroke Kick
+       <Container>
+       <br></br>
+       <br></br>
+        <h1>Advanced</h1>
+        <br></br>
+        <br></br>
+        <div className='div-one'>
+         <p> Breaststroke Kick
          
-//             <Rating 
-//                 // className='rating'
-//                 style={{marginLeft:50}}
-//                 value={value} 
-//                 onChange={(e, newValue) => {
-//                 setValue(newValue);
-//               }} 
-//                 onChangeActive={(e, newHover) => {
-//                 setHover(newHover);
-//               }} >   
-//             </Rating>  
-//             </p>
-//             </div>       
-//             <hr/>
+            <Rating 
+                // className='rating'
+                style={{marginLeft:50}}
+                value={value} 
+                onChange={(e, newValue) => {
+                setValue(newValue);
+              }} 
+                onChangeActive={(e, newHover) => {
+                setHover(newHover);
+              }} >   
+            </Rating>  
+            </p>
+            </div>       
+            <hr/>
            
-//           <p>Breaststroke Pull
-//             <Rating 
-//             style={{marginLeft:40, marginBottom:10}}
-//                 value={value} 
-//                 onChange={(e, newValue) => {
-//                 setValue(newValue);
-//             }} 
-//                 onChangeActive={(e, newHover) => {
-//                 setHover(newHover);
-//             }}    
-//             ></Rating></p> 
+          <p>Breaststroke Pull
+            <Rating 
+            style={{marginLeft:40, marginBottom:10}}
+                value={value} 
+                onChange={(e, newValue) => {
+                setValue(newValue);
+            }} 
+                onChangeActive={(e, newHover) => {
+                setHover(newHover);
+            }}    
+            ></Rating></p> 
            
         
-//             <hr/>
-//           <p>Butterfly Kick (Dolphin Kick)
-//             <Rating 
-//                 style={{marginLeft:50}}
-//                 value={value} 
-//                 onChange={(e, newValue) => {
-//                 setValue(newValue);
-//             }} 
-//                 onChangeActive={(e, newHover) => {
-//                  setHover(newHover);
-//             }}    
-//            ></Rating></p> 
-//             <hr/>
-//           <p>Basic Drills
-//             <Rating 
-//                 style={{marginLeft:50}}
-//                  value={value} 
-//                  onChange={(e, newValue) => {
-//                  setValue(newValue);
-//             }} 
-//                 onChangeActive={(e, newHover) => {
-//                 setHover(newHover);
-//             }}    
-//             ></Rating></p> 
+            <hr/>
+          <p>Butterfly Kick (Dolphin Kick)
+            <Rating 
+                style={{marginLeft:50}}
+                value={value} 
+                onChange={(e, newValue) => {
+                setValue(newValue);
+            }} 
+                onChangeActive={(e, newHover) => {
+                 setHover(newHover);
+            }}    
+           ></Rating></p> 
+            <hr/>
+          <p>Basic Drills
+            <Rating 
+                style={{marginLeft:50}}
+                 value={value} 
+                 onChange={(e, newValue) => {
+                 setValue(newValue);
+            }} 
+                onChangeActive={(e, newHover) => {
+                setHover(newHover);
+            }}    
+            ></Rating></p> 
     
-//             <hr/>
-//          <p>Full Lap Legal Breaststroke
-//             <Rating 
-//                 style={{marginLeft:50}}
-//                 value={value} 
-//                 onChange={(e, newValue) => {
-//                 setValue(newValue);
-//             }} 
-//                 onChangeActive={(e, newHover) => {
-//                 setHover(newHover);
-//             }}    
-//             ></Rating></p> 
+            <hr/>
+         <p>Full Lap Legal Breaststroke
+            <Rating 
+                style={{marginLeft:50}}
+                value={value} 
+                onChange={(e, newValue) => {
+                setValue(newValue);
+            }} 
+                onChangeActive={(e, newHover) => {
+                setHover(newHover);
+            }}    
+            ></Rating></p> 
             
-//             <hr/>
-//          <p>Full Lap Legal Backstroke
-//             <Rating 
-//                 style={{marginLeft:50}}
-//                 value={value} 
-//                 onChange={(e, newValue) => {
-//                 setValue(newValue);
-//             }} 
-//                 onChangeActive={(e, newHover) => {
-//                 setHover(newHover);
-//             }}    
-//          ></Rating></p>   
-//          <hr/>
-//                   <p>Full Lap Butterfly
-//             <Rating 
-//                 style={{marginLeft:50}}
-//                 value={value} 
-//                 onChange={(e, newValue) => {
-//                 setValue(newValue);
-//             }} 
-//                 onChangeActive={(e, newHover) => {
-//                 setHover(newHover);
-//             }}    
-//          ></Rating></p>   
-//          <hr/>
-//          <p>Full Lap Freestyle
-//             <Rating 
-//                 style={{marginLeft:50}}
-//                 value={value} 
-//                 onChange={(e, newValue) => {
-//                 setValue(newValue);
-//             }} 
-//                 onChangeActive={(e, newHover) => {
-//                 setHover(newHover);
-//             }}    
-//          ></Rating></p>   
-//          <hr/>
-//                 <form input='text'>Comments/Notes</form>
-//                 <textarea placeholder='Comments/Notes'></textarea>
-//                 {/* <Button> Add </Button> */}
+            <hr/>
+         <p>Full Lap Legal Backstroke
+            <Rating 
+                style={{marginLeft:50}}
+                value={value} 
+                onChange={(e, newValue) => {
+                setValue(newValue);
+            }} 
+                onChangeActive={(e, newHover) => {
+                setHover(newHover);
+            }}    
+         ></Rating></p>   
+         <hr/>
+                  <p>Full Lap Butterfly
+            <Rating 
+                style={{marginLeft:50}}
+                value={value} 
+                onChange={(e, newValue) => {
+                setValue(newValue);
+            }} 
+                onChangeActive={(e, newHover) => {
+                setHover(newHover);
+            }}    
+         ></Rating></p>   
+         <hr/>
+         <p>Full Lap Freestyle
+            <Rating 
+                style={{marginLeft:50}}
+                value={value} 
+                onChange={(e, newValue) => {
+                setValue(newValue);
+            }} 
+                onChangeActive={(e, newHover) => {
+                setHover(newHover);
+            }}    
+         ></Rating></p>   
+         <hr/>
+                <form input='text'>Comments/Notes</form>
+                <textarea placeholder='Comments/Notes'></textarea>
+                {/* <Button> Add </Button> */}
                 
          
             
-//         </Container>
-//     )
-// }
+        </Container>
+    )
+}
 
 export default Advanced
--- src/Beginners.js
+++ src/Beginners.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect }  from 'react';
 
 import {Rating} from '@material-ui/lab';
 import {Button} from 'reactstrap'
-import {Card, CardTitle, CardText, Row, Col} from 'reactstrap'
+import {Card, CardTitle, CardText, Row, Col, Table} from 'reactstrap'
 import { Container } from 'reactstrap';
-// import './Beginners.css';
+import './Beginners.css';
 import './Navbar';
 import { blue, green } from '@material-ui/core/colors';
 
@@ -104,6 +104,184 @@ function Beginners() {
                 setHover(newHover);
             }}    
          ></Rating></p> 
+         <Table  bordered class="table table-hover " className='table'>
+  <thead>
+    <tr>
+      {/* <th scope="col">#</th> */}
+      <th scope="col">Achievement</th>
+      <th scope="col">First Accessment</th>
+      <th scope="col">Last Accessment</th>
+      <th scope="col">Total/Average</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      {/* <th scope="row">1</th> */}
+      <td>Face In Water (3 seconds)</td>
+      <td>            <Rating 
+                style={{marginLeft:-6}}
+                value={value} 
+                onChange={(e, newValue) => {
+                setValue(newValue);
+            }} 
+                onChangeActive={(e, newHover) => {
+                setHover(newHover);
+            }}    
+         ></Rating></td>
+      <td><Rating 
+                style={{marginLeft:-6}}
+                value={value} 
+                onChange={(e, newValue) => {
+                setValue(newValue);
+            }} 
+                onChangeActive={(e, newHover) => {
+                setHover(newHover);
+            }}    
+         ></Rating></td>
+         <th scope="col"></th>
+    </tr>
+  </tbody>
+  <tbody>
+    <tr>
+      {/* <th scope="row">1</th> */}
+     {/* row 2  */}
+      <td>Glide on Front (face in water 3 seconds)</td>
+      <td>            <Rating 
+                style={{marginLeft:-6}}
+                value={value} 
+                onChange={(e, newValue) => {
+                setValue(newValue);
+            }} 
+                onChangeActive={(e, newHover) => {
+                setHover(newHover);
+            }}    
+         ></Rating></td>
+      <td><Rating 
+                style={{marginLeft:-6}}
+                value={value} 
+                onChange={(e, newValue) => {
+                setValue(newValue);
+            }} 
+                onChangeActive={(e, newHover) => {
+                setHover(newHover);
+            }}    
+         ></Rating></td>
+         <th scope="col"></th>
+    </tr>
+  </tbody>
+  <tbody>
+    <tr>
+      {/* <th scope="row">1</th> */}
+      {/* row 3 */}
+      <td>Flutter Kick (face down 3 seconds)</td>
+      <td>            <Rating 
+                style={{marginLeft:-6}}
+                value={value} 
+                onChange={(e, newValue) => {
+                setValue(newValue);
+            }} 
+                onChangeActive={(e, newHover) => {
+                setHover(newHover);
+            }}    
+         ></Rating></td>
+      <td><Rating 
+                style={{marginLeft:-6}}
+                value={value} 
+                onChange={(e, newValue) => {
+                setValue(newValue);
+            }} 
+                onChangeActive={(e, newHover) => {
+                setHover(newHover);
+            }}    
+         ></Rating></td>
+         <th scope="col"></th>
+    </tr>
+  </tbody>
+  <tbody>
+    <tr>
+      {/* <th scope="row">1</th> */}
+      {/* row 4 */}
+      <td>Float on Back (5-10 seconds assisted)</td>
+      <td>            <Rating 
+                style={{marginLeft:-6}}
+                value={value} 
+                onChange={(e, newValue) => {
+                setValue(newValue);
+            }} 
+                onChangeActive={(e, newHover) => {
+                setHover(newHover);
+            }}    
+         ></Rating></td>
+      <td><Rating 
+                style={{marginLeft:-6}}
+                value={value} 
+                onChange={(e, newValue) => {
+                setValue(newValue);
+            }} 
+                onChangeActive={(e, newHover) => {
+                setHover(newHover);
+            }}    
+         ></Rating></td>
+         <th scope="col"></th>
+    </tr>
+  </tbody>
+  <tbody>
+    <tr>
+      {/* <th scope="row">1</th> */}
+      {/* row 5 */}
+      <td>Glide on Back, add Flutter Kick</td>
+      <td>            <Rating 
+                style={{marginLeft:-6}}
+                value={value} 
+                onChange={(e, newValue) => {
+                setValue(newValue);
+            }} 
+                onChangeActive={(e, newHover) => {
+                setHover(newHover);
+            }}    
+         ></Rating></td>
+      <td><Rating 
+                style={{marginLeft:-6}}
+                value={value} 
+                onChange={(e, newValue) => {
+                setValue(newValue);
+            }} 
+                onChangeActive={(e, newHover) => {
+                setHover(newHover);
+            }}    
+         ></Rating></td>
+         <th scope="col"></th>
+    </tr>
+  </tbody>
+  <tbody>
+    <tr>
+      {/* <th scope="row">1</th> */}
+      {/* row 6 */}
+      <td>Glide on Back, add Flutter Kick</td>
+      <td>            <Rating 
+                style={{marginLeft:-6}}
+                value={value} 
+                onChange={(e, newValue) => {
+                setValue(newValue);
+            }} 
+                onChangeActive={(e, newHover) => {
+                setHover(newHover);
+            }}    
+         ></Rating></td>
+      <td><Rating 
+                style={{marginLeft:-6}}
+                value={value} 
+                onChange={(e, newValue) => {
+                setValue(newValue);
+            }} 
+                onChangeActive={(e, newHover) => {
+                setHover(newHover);
+            }}    
+         ></Rating></td>
+         <th scope="col"></th>
+    </tr>
+  </tbody>
+  </Table>
                
          <hr/>
                 <form input='text'>Comments/Notes</form>
